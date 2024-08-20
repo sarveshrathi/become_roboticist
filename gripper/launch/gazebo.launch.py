043e3cdb70dfcbe2dabf8482e69b845bf1b7055b
Sarvesh <rathisarvesh995@gmail.com>
2024-08-20 23:24:58 +0000

    latest changes

---
+++ gripper/launch/gazebo.launch.py
@@ -0,0 +1,100 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.conditions import UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    resources_package = 'gripper'
+
+    # Make path to resources dir without last package_name fragment.
+    path_to_share_dir_clipped = ''.join(get_package_share_directory(resources_package).rsplit('/' + resources_package, 1))
+
+    # Gazebo hint for resources.
+    os.environ['GZ_SIM_RESOURCE_PATH'] = path_to_share_dir_clipped
+
+    # Ensure `SDF_PATH` is populated since `sdformat_urdf` uses this rather
+    # than `GZ_SIM_RESOURCE_PATH` to locate resources.
+    if "GZ_SIM_RESOURCE_PATH" in os.environ:
+        gz_sim_resource_path = os.environ["GZ_SIM_RESOURCE_PATH"]
+
+        if "SDF_PATH" in os.environ:
+            sdf_path = os.environ["SDF_PATH"]
+            os.environ["SDF_PATH"] = sdf_path + ":" + gz_sim_resource_path
+        else:
+            os.environ["SDF_PATH"] = gz_sim_resource_path
+
+    # Gazebo Sim.
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments=dict(gz_args='-r empty.sdf --verbose').items(),
+        )
+
+    # Spawn
+    spawn = Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                '-name', 'Gripper.urdf',
+                '-x', '1.2',
+                '-z', '2.3',
+                '-Y', '3.4',
+                '-topic', '/robot_description'
+                ],
+            output='screen',
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    use_sim_time_launch_arg = DeclareLaunchArgument('use_sim_time', default_value='true')
+
+    use_rviz = LaunchConfiguration('use_rviz')
+
+    use_rviz_arg = DeclareLaunchArgument("use_rviz", default_value='true')
+
+    robot_state_publisher = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare(resources_package),
+                    'launch',
+                    'description.launch.py'
+                    ])
+                ]),
+            condition=UnlessCondition(use_rviz),  # rviz launch includes rsp.
+            launch_arguments=dict(use_sim_time=use_sim_time).items(),
+            )
+
+    rviz = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare(resources_package),
+                'launch',
+                'display.launch.py'
+            ])
+        ]),
+        condition=IfCondition(use_rviz),
+        )
+
+    return LaunchDescription([
+        use_sim_time_launch_arg,
+        use_rviz_arg,
+        robot_state_publisher,
+        rviz,
+        gazebo,
+        spawn,
+    ])
--- maciv2_moveit/launch/move_group.launch.py
+++ maciv2_moveit/launch/move_group.launch.py
@@ -1,7 +1,28 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
 
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("maciv2", package_name="maciv2_moveit").to_moveit_configs()
     return generate_move_group_launch(moveit_config)
+
+'''    
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
+            {"publish_robot_description_semantic": True},
+            {"use_sim_time": True},
+        ],
+    )
+
+    return LaunchDescription(
+        [move_group_node]
+    )
+'''
